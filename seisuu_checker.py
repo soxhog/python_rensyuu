73110c462c41079d48bf2bc82439da0da10022f4
skagiya <>
2020-03-08 17:34:18 +0900

    first_checker

---
+++ seisuu_checker.py
@@ -0,0 +1,16 @@
+import sys
+
+while 1:
+	print ("\n整数xを入力してください。")
+	try:
+		x = int(input("x = "))
+	except ValueError:
+		print("\nError")
+		continue
+	break
+if x < 0:
+	print('xは負の整数です')
+elif x > 0:
+	print('xは正の整数です')
+else:
+	print('xは0です')
